0076dc99b7a03d5c4665e90094d1ec4a33d1b3b9
Kurniawanahama20 <umykurniawanahama@gmail.com>
2026-03-10 19:33:09 +0700

    commit keempat

--- file2.cpp
+++ file2.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 
 float Rata_rata(float a, float b){
-    return (a=b)/2
+    return (a=b)/2;
+}
+
+string Status_lulus(float r) {
+    if (r >= 60)
+        return "lolos";
 }
