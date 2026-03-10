320ef7596c150a7c92a30459e37442d495791847
Kurniawanahama20 <umykurniawanahama@gmail.com>
2026-03-10 19:02:03 +0700

    commit kedua

--- file1.cpp
+++ file1.cpp
@@ -6,5 +6,11 @@ int main(){
 
     srand(time(0));
     x = rand() %10;
-    
+
+    if (x==5){
+        x = x=1;
+        cout<<"nilai x sudah di koreksi menjadi : "<<x<<endl;
+    }
+
+    cout << "bilangan yang diberikan = "<<x;
 }
